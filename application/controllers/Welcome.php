882a23520e23a878591e6a7a8c34cc73bbd49295
bluewave41 <megaman22342@live.ca>
2016-02-14 15:44:30 -0800

    Base structure

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -1,25 +1,54 @@
 <?php
-defined('BASEPATH') OR exit('No direct script access allowed');
 
-class Welcome extends CI_Controller {
+class Welcome extends Application {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
-	{
-		$this->load->view('welcome_message');
+	function __construct() {
+		parent::__construct();
 	}
+
+	//-------------------------------------------------------------
+	//  The normal pages
+	//-------------------------------------------------------------
+
+	function index() {
+		$this->data['pagebody'] = 'homepage';	// this is the view we want shown
+
+		$source = $this->players->all();
+        $source2 = $this->stocks->all();
+                
+		$portfolios = array();
+        $stockportfolios = array();
+                
+		foreach ($source as $record) {
+			$portfolios[] = array('who' => $record['who'], 'mug' => $record['mug'], 'href' => $record['where']);
+		}
+		$this->data['portfolios'] = $portfolios;
+                
+        foreach ($source2 as $record2) {
+			$stockportfolios[] = array('who' => $record2['who'], 'mug' => $record2['mug'], 'href' => $record2['where']);
+		}
+		$this->data['stockportfolios'] = $stockportfolios;
+
+		$this->render();
+	}
+    
+    function stock($id){
+        $record2 = $this->stocks->data[$id-1];
+        $this->data = array_merge($this->data, $record2);
+        $this->data['pagebody'] = 'history';
+
+        $this->render();
+    }
+
+    function player($id){
+        $record = $this->players->data[$id-1];
+        $this->data = array_merge($this->data, $record);
+        $this->data['pagebody'] = 'portfolio';
+
+        $this->render();
+    }
+
 }
+
+/* End of file Welcome.php */
+/* Location: application/controllers/Welcome.php */
