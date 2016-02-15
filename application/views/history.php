ba93185b1e91792f1edf8048878e995d2cc26515
bluewave41 <megaman22342@live.ca>
2016-02-14 22:27:46 -0800

    Fixed drop down boxes.

--- application/views/history.php
+++ application/views/history.php
@@ -7,5 +7,8 @@
 		<option value="1">{one}</option>
 		<option value="2">{two}</option>
 		<option value="3">{three}</option>
+		<option value="4">{four}</option>
+		<option value="5">{five}</option>
+		<option value="6">{six}</option>
 	</select>
 </div>
